d5de4f8afd414ec58a828b3d373f4a2c0eea54f0
harika <e.harika78@gmail.com>
2020-01-13 17:44:15 +0530

    password validation

--- src/main/java/ImageHoster/ImageHosterApplication.java
+++ src/main/java/ImageHoster/ImageHosterApplication.java
@@ -1,13 +1,21 @@
 package ImageHoster;
 
+import ImageHoster.validators.PasswordValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ImageHosterApplication {
+    @Bean
+    public PasswordValidator validator() {
+        return new PasswordValidator();
+    }
     public static void main(String[] args) {
         SpringApplication.run(ImageHosterApplication.class, args);
+
     }
 }
